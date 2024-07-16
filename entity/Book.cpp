3c81ffd0ec91f5197f783f557124785512f205c9
Ossowitz <76myxomor76@gmail.com>
2024-07-16 15:00:00 +0300

    CW-01

--- entity/Book.cpp
+++ entity/Book.cpp
@@ -1,1 +1,14 @@
 #include "Book.h"
+
+#include <iostream>
+
+using namespace std;
+
+Book::Book(const string& title, const string& author, const string& genre)
+    : title(title), author(author), genre(genre) {}
+
+void Book::display() const {
+    cout << "Title: " << title
+    << ", Author: " << author
+    << ", Genre: " << genre;
+}
