c350a00dc5eaca1d62a79300d681e2159e4a366b
Ossowitz <76myxomor76@gmail.com>
2024-07-16 16:00:00 +0300

    CW-02

--- entity/Book.cpp
+++ entity/Book.cpp
@@ -4,11 +4,19 @@
 
 using namespace std;
 
-Book::Book(const string& title, const string& author, const string& genre)
-    : title(title), author(author), genre(genre) {}
+Book::Book(const std::string& t, const std::string& a, const std::string& g)
+    : title(t), author(a), genre(g), currentBorrowerID(""), borrowDate(""), returnDate("") {}
 
 void Book::display() const {
     cout << "Title: " << title
     << ", Author: " << author
     << ", Genre: " << genre;
+
+    if (currentBorrowerID.empty()) {
+        cout << ", Status: Available" << endl;
+    } else {
+        cout << ", Status: Currently borrowed by Reader ID: " << currentBorrowerID
+        << ", Borrowed on: " << borrowDate
+        << ", Return by: " << returnDate << endl;
+    }
 }
