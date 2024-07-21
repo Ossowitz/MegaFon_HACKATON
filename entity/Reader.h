5c2f2763b2581d8557993fd8154e27674c517112
Ossowitz <76myxomor76@gmail.com>
2024-07-21 14:00:00 +0300

    CW-07

--- entity/Reader.h
+++ entity/Reader.h
@@ -1,12 +1,18 @@
 #ifndef READER_H
 #define READER_H
 
+#include <string>
+#include <vector>
+#include <iomanip>
 
+using namespace std;
 
 class Reader {
-
+public:
+    string passport;
+    string name;
+    string readerId;
+    vector<string> borrowedBookTitles;
 };
-
-
 
 #endif //READER_H
