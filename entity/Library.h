cbfc90b41be0e36e4a0e26d33da02878a5459da1
Ossowitz <76myxomor76@gmail.com>
2024-07-26 14:00:00 +0300

    CW-15

---
+++ entity/Library.h
@@ -0,0 +1,16 @@
+#ifndef LIBRARY_H
+#define LIBRARY_H
+
+#include <vector>
+#include <string>
+#include <filesystem>
+#include "Book.h"
+#include "Reader.h"
+
+class Library {
+
+};
+
+
+
+#endif //LIBRARY_H
