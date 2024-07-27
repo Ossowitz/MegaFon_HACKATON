56e7ec735d6966d96c4b295beacaba7ce0842d23
Ossowitz <76myxomor76@gmail.com>
2024-07-27 14:00:00 +0300

    CW-16

--- entity/Library.h
+++ entity/Library.h
@@ -7,10 +7,24 @@
 #include "Book.h"
 #include "Reader.h"
 
+using namespace std;
+
 class Library {
+private:
+    vector<Book> books;
+    vector<Reader> readers;
 
+public:
+    void addBook(const Book& book);
+    void addReader(const Reader& reader);
+    void loadBooksFromFile(const std::filesystem::path& filepath);
+    void loadReadersFromFile(const std::filesystem::path& filepath);
+    void saveBooksToFile(const std::filesystem::path& filepath) const;
+    void saveReadersToFile(const std::filesystem::path& filepath) const;
+    void displayBooks() const;
+    void displayReaders() const;
+    Book* findBook(const std::string& title);
+    Reader* findReader(const std::string& id);
 };
-
-
 
 #endif //LIBRARY_H
